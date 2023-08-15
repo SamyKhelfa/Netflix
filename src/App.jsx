eb8dfefcf6f9ec5c0c23fbed90affd4d65425d0a
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-08-15 17:22:43 +0200

    update size images

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,24 @@ import data from "./assets/movies_rnexgr.json";
 
 function App() {
   return (
-    <>
+    <div className="font">
       <img
         className="logo"
         src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
       />
-      {/* <h1>Emissions TV</h1>
-      <h1>Séries françaises</h1>
-      <h1>Comédies</h1>
-      <h1>Nouveautés</h1>
-      <h1>Documentaires</h1>
-      <h1>Séries US</h1> */}
-      <div>
-        {
-          data.map((item, index) => {
-            return (
-              <div>
-                <h2>{item.category}</h2>
-                <img src={item.images[index]} alt="" />
-              </div>
-            );
-          })
-          /* {data.map((item, index) =>
-          item.images.map((imgSrc, i) => (
-            <img key={`${index}-${i}`} src={imgSrc} alt="" />
-          ))
-        )} */
-        }
-      </div>
-    </>
+      {data.map((item) => {
+        return (
+          <div className="container" key={item.category}>
+            <h2>{item.category}</h2>
+            <div className="movies">
+              {item.images.map((imgSrc, index) => {
+                return <img className="element" key={index} src={imgSrc} />;
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
